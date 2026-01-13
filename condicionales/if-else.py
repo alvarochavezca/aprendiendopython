a2b23f0de23f89970d1f7c8a105c02b88fe0f9cf
alvaro chavez <alvaro@gmail.com>
2026-01-13 12:58:58 -0500

    subiendo varios codigos

---
+++ condicionales/if-else.py
@@ -0,0 +1,17 @@
+
+contraseña_almacenada = "DaltoMaestro"
+contraseña_escrita = "DaltoMaestro" 
+
+if contraseña_almacenada == contraseña_escrita:
+    print("INCIANDO SESION....")
+    
+else:
+    print("CONTRASEÑA EQUIVOCADA,INTENTE DE NUEVO")
+
+
+
+
+
+
+
+
---
+++ metodos/metodos_cadenas.py
@@ -0,0 +1,19 @@
+cadena1 = "hola soy dalto"
+cadena2 = "bienvenido maquinola"
+
+#convierte a u mayusculas
+mayusc = cadena1.upper() 
+
+#convierte a minusuculas
+minusc = cadena1.lower()
+
+#primera letra en mayuscula
+primera_letra_mayusc = cadena1.capitalize()
+
+#buscamos una cadena en otra cadena, si no hay conincidencia devuelve -1
+busqueda_find = cadena1.find("o")
+
+#buscamos una cadena en otra cadena
+busqueda_index = cadena1.index("d")
+
+print(busqueda_index)
